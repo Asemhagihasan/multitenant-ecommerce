f239aa07295d84e0792ac005ba6541908b058b38
AsemHagiHasan <asemhagihasan12345@gmail.com>
2025-09-07 21:38:09 +0300

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <Button>Button</Button>
+    <div className="flex flex-col gap-4 p-4">
+      <div>
+        <Button variant="elevated">Button</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea value="textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 };

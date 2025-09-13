2a94d10d9e4540dcf2912f334aeba39fe54946e0
AsemHagiHasan <asemhagihasan12345@gmail.com>
2025-09-13 17:16:25 +0300

    08: authentication

---
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import SignInView from "@/modules/ui/views/sign-in-view";
+import React from "react";
+
+const SignInPage = () => {
+  return <SignInView />;
+};
+
+export default SignInPage;
---
+++ src/modules/ui/views/sign-up-view.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+import { RegisterSchema } from "@/modules/auth/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const popins = Poppins({ subsets: ["latin"], weight: "700" });
+
+const SignUpView = () => {
+  const router = useRouter();
+  const trpc = useTRPC();
+  const { mutate: register, isPending: isLoading } = useMutation(
+    trpc.auth.register.mutationOptions()
+  );
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    register(values, {
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: () => {
+        toast.success("Account created successfully");
+        router.push("/");
+      },
+    });
+  };
+
+  const username = form.watch("username");
+
+  const usernameErrors = form.formState.errors.username;
+
+  const showPreview = username && !usernameErrors;
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5">
+      <div className="bg-[#F4F4F0] h-screen w-full lg:col-span-3 overflow-y-auto">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-8 p-4 lg:p-16"
+          >
+            <div className="flex items-center justify-between mb-8">
+              <Link href="/">
+                <span
+                  className={cn("text-2xl font-semibold", popins.className)}
+                >
+                  funroad
+                </span>
+              </Link>
+              <Button
+                asChild
+                variant="ghost"
+                size="sm"
+                className="text-base border-none underline"
+              >
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+            </div>
+            <h1 className="text-4xl font-medium">
+              Join over 1000 creatores earning from on Funroad
+            </h1>
+            <FormField
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription
+                    className={cn("hidden", showPreview && "block")}
+                  >
+                    Your store will be available at&nbsp;{" "}
+                    <strong>{username}</strong>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              size="lg"
+              variant="elevated"
+              className="bg-black text-white hover:bg-pink-400"
+              disabled={isLoading}
+            >
+              Create account
+            </Button>
+          </form>
+        </Form>
+      </div>
+      <div
+        className="h-screen w-full lg:col-span-2 hidden lg:block"
+        style={{
+          backgroundImage: "url('/auth-bg.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+        }}
+      />
+    </div>
+  );
+};
+
+export default SignUpView;
